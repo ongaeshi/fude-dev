ddcb16dcc05d0c6e3c76a5438c41b31ee69dd115
ongaeshi <ongaeshi0621@gmail.com>
2019-08-16 00:36:32 +0900

    refactor: Divide to mrb_raylib.[ch]

--- mruby-vs2017/Main.cpp
+++ mruby-vs2017/Main.cpp
@@ -1,15 +1,8 @@
-﻿#include "pch.h"
+﻿#include "mrb_raylib.h"
+
 #include "mruby.h"
 #include "mruby/compile.h"
-#include "mruby/class.h"
-#include "mruby/data.h"
-#include "mruby/value.h"
-#include "raylib.h"
 #include <string.h>
-
-extern void mrb_raylib_module_init(mrb_state *mrb);
-extern mrb_value mrb_raylib_vector3_initialize(mrb_state *mrb, mrb_value self);
-extern mrb_value mrb_raylib_camera_initialize(mrb_state *mrb, mrb_value self);
 
 int main()
 {
@@ -23,51 +16,5 @@ int main()
 	fclose(fp);
 
 	mrb_close(mrb);
-}
-
-// struct Vector3
-// struct Camera
-void mrb_raylib_module_init(mrb_state *mrb)
-{
-	struct RClass *mod_raylib = mrb_define_module(mrb, "Raylib");
-	struct RClass *raylib_error_cls = mrb_define_class_under(mrb, mod_raylib, "RaylibError", mrb->eStandardError_class);
-
-	{
-		struct RClass *cls = mrb_define_class_under(mrb, mod_raylib, "Vector3", mrb->object_class);
-		MRB_SET_INSTANCE_TT(cls, MRB_TT_DATA);
-		mrb_define_method(mrb, cls, "initialize", mrb_raylib_vector3_initialize, MRB_ARGS_NONE());
-	}
-
-	{
-		struct RClass *cls = mrb_define_class_under(mrb, mod_raylib, "Camera", mrb->object_class);
-		MRB_SET_INSTANCE_TT(cls, MRB_TT_DATA);
-		mrb_define_method(mrb, cls, "initialize", mrb_raylib_camera_initialize, MRB_ARGS_NONE());
-	}
-}
-
-const static struct mrb_data_type mrb_raylib_vector3_type = { "Vector3", mrb_free };
-
-mrb_value mrb_raylib_vector3_initialize(mrb_state *mrb, mrb_value self)
-{
-	Vector3 *p;
-
-	p = (Vector3*)mrb_malloc(mrb, sizeof(Vector3));
-	memset(p, 0, sizeof(Vector3));
-	DATA_TYPE(self) = &mrb_raylib_vector3_type;
-	DATA_PTR(self) = p;
-	return self;
-}
-
-const static struct mrb_data_type mrb_raylib_camera_type = { "Camera", mrb_free };
-
-mrb_value mrb_raylib_camera_initialize(mrb_state *mrb, mrb_value self)
-{
-	Camera *p;
-
-	p = (Camera*)mrb_malloc(mrb, sizeof(Camera));
-	memset(p, 0, sizeof(Camera));
-	DATA_TYPE(self) = &mrb_raylib_camera_type;
-	DATA_PTR(self) = p;
-	return self;
 }
 
---
+++ mruby-vs2017/mrb_raylib.h
@@ -0,0 +1,22 @@
+﻿#ifndef MRB_RAYLIB__H
+#define MRB_RAYLIB__H
+
+#include <mruby.h>
+
+#ifdef __cplusplus
+#define MRB_RAYLIB_API extern "C"
+#else
+#define MRB_RAYLIB_API
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+MRB_RAYLIB_API void mrb_raylib_module_init(mrb_state *mrb);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MRB_RAYLIB__H
