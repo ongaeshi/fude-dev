af0a7d758d37a61786d4d8d257f90e9917ac1c40
ongaeshi <ongaeshi0621@gmail.com>
2019-09-10 02:27:49 +0900

    feat: Show ruby exception message

--- mruby-vs2017/Main.cpp
+++ mruby-vs2017/Main.cpp
@@ -13,6 +13,11 @@ int main()
 	FILE* fp;
 	fopen_s(&fp, "main.rb", "r");
 	mrb_value ret = mrb_load_file(mrb, fp);
+	
+	if (mrb->exc) {
+		mrb_p(mrb, mrb_obj_value(mrb->exc));
+	}
+
 	fclose(fp);
 
 	mrb_close(mrb);
