1c02b230b816501cf8a5a3e8673629dbd2b10cd5
ongaeshi <ongaeshi0621@gmail.com>
2019-11-19 01:12:54 +0900

    feat: Script file name can also be set from the argument

--- mruby-vs2017/Main.cpp
+++ mruby-vs2017/Main.cpp
@@ -4,14 +4,20 @@
 #include "mruby/compile.h"
 #include <string.h>
 
-int main()
+int main(int argc, char* argv[])
 {
+	const char* fileName = "main.rb";
+
+	if (argc > 1) {
+		fileName = argv[1];
+	}
+
 	mrb_state* mrb = mrb_open();
 
 	mrb_raylib_module_init(mrb);
 
 	FILE* fp;
-	fopen_s(&fp, "main.rb", "r");
+	fopen_s(&fp, fileName, "r");
 	mrb_value ret = mrb_load_file(mrb, fp);
 	
 	if (mrb->exc) {
